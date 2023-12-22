f5fc8d1a4089dfc103b2b3dd8ade38053d6599f9
junayet <muhammadjunayetmaruf@gmail.com>
2023-12-22 15:05:44 +0600

    task can be editable, delateable and customizble

--- index.js
+++ index.js
@@ -22,10 +22,7 @@ async function run() {
     try {
         const taskSphere = client.db('taskSphere')
         const usersCollection = taskSphere.collection('users')
-        
-
-       
-        
+        const taskCollection = taskSphere.collection('task')
 
         // usersCollection start
         app.post('/users', async (req, res) => {
@@ -38,20 +35,90 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result)
         })
-       
-       
+
+
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result)
         })
-        app.get('/users/:email',  async (req, res) => {
+        app.get('/users/:email', async (req, res) => {
             const email = req?.params?.email
             const query = { email: email }
             const result = await usersCollection.findOne(query);
             res.send(result)
         })
-        
-        
+        // usersCollection end 
+
+        // taskCollection start
+        app.get('/tasks', async (req, res) => {
+            const result = await taskCollection.find().toArray()
+            res.send(result)
+        })
+        app.post('/tasks', async (req, res) => {
+            const task = req?.body;
+            const result = await taskCollection.insertOne(task)
+            res.send(result)
+        })
+        app.get('/tasks/:email', async (req, res) => {
+            const email = req?.params?.email;
+            const query = { creator: email }
+            const result = await taskCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.put('/tasksOngoing/:id', async (req, res) => {
+            const id = req?.params?.id;
+            const query = { _id: new ObjectId(id) }
+            const updatedData = {
+                $set: {
+                    type: 'ongoing'
+                }
+            }
+            console.log(query);
+            const result = await taskCollection.updateOne(query, updatedData)
+            res.send(result)
+        })
+        app.put('/tasksCompleted/:id', async (req, res) => {
+            const id = req?.params?.id;
+            const query = { _id: new ObjectId(id) }
+            const updatedData = {
+                $set: {
+                    type: 'completed'
+                }
+            }
+            const result = await taskCollection.updateOne(query, updatedData)
+            res.send(result)
+        })
+        app.put('/singletaskupdate/:id', async (req, res) => {
+            const data = req?.body
+            const id = req?.params?.id;
+            const query = { _id: new ObjectId(id) }
+            const updatedtask = {
+                $set: {
+                    title: data?.title,
+                    deadline: data?.deadline,
+                    description: data?.description,
+                    priority: data?.priority
+
+                }
+            }
+            const result = await taskCollection.updateOne(query, updatedtask)
+            res.send(result)
+        })
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req?.params?.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.deleteOne(query)
+            res.send(result)
+        })
+        app.get('/singletask/:id', async (req, res) => {
+            const id = req?.params?.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.findOne(query)
+            res.send(result)
+        })
+        // taskCollection end
+
+
     }
     finally {
 
